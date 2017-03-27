3c4663abf5f16862e66aeec01049fd1d205a4d7e
Julian Kyer <julian.kyer@gmail.com>
2017-03-27 15:52:30 -0600

    Router lesson without redux

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import CreateItem from './createItem.js';
 import { NavBar }from './navBar.js';
 import { Home }from './home.js';
 import { ListItem }from './listItem.js';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      ideas: [{title: 'cool', body: 'yay', id: Date.now()}, {title: 'asld;kfj', body: 'yay', id: Date.now()+1}]
+      ideas: [{title: 'cool', body: 'yay', id: Date.now()}, {title: 'asld;kfj', body: 'yay', id: Date.now()+1}],
+      thoughts: []
     };
   }
 
@@ -19,21 +21,71 @@ submitIdea(idea) {
   this.setState({ ideas: ideas});
 }
 
+submitThought(thought) {
+  const { thoughts } = this.state;
+  thoughts[thoughts.length] = { ...thought, id: Date.now() }
+  this.setState({ thoughts: thoughts});
+}
+
   render() {
-    const { ideas } = this.state;
+    const { ideas, thoughts } = this.state;
 
     return (
-      <div className='App'>
-        <div className='App-header'>
-          <div className='App-title'>Idea & Thought Box</div>
-          <NavBar />
+      <Router>
+        <div className='App'>
+          <div className='App-header'>
+            <div className='App-title'>Idea & Thought Box</div>
+            <NavBar />
+          </div>
+          
+          <Route exact path='/' component={Home}/>
+          
+          <Route path='/create-idea' render={ () => 
+            <CreateItem submitForm={this.submitIdea.bind(this)}/>
+            } />
+            
+          <Route path='/ideas' render={(props) => {
+            return (
+              <div className='list'>
+                {ideas.map((idea) => <ListItem key={idea.id} {...idea}/>)}
+              </div>
+            )  
+          }} />
+          
+        <Route path='/ideas/:ideaId' render={({match}) => {
+            const idea = ideas.find((idea) => idea.id === parseInt(match.params.ideaId))
+            if (idea) {
+              return <ListItem {...idea}/>
+            }
+            return (
+              <div className='list-item'>Fuck off</div>
+            )
+          }} />
+          
+        <Route path='/create-thought' render={ () => {
+          return <CreateItem submitForm={this.submitThought.bind(this)} />
+          }} />
+          
+        <Route path='/list-thoughts' render={() => {
+            return (
+              <div className='list'>
+                {thoughts.map((thought) => <ListItem key={thought.id} {...thought}/>)}
+              </div>
+            )
+          }}/>
+          
+        <Route path='/thoughts/:thoughtId' render={({match}) => {
+              const thought = thoughts.find((thought) => thought.id === parseInt(match.params.thoughtId))
+              if (thought) {
+                return <ListItem {...thought}/>
+              }
+              return (
+                <div className='list-item'>Fuck off</div>
+              )
+            }} />
+          
         </div>
-        <CreateItem submitForm={this.submitIdea.bind(this)}/>
-
-        <div className='list'>
-          {ideas.map((idea) => <ListItem key={idea.id} {...idea}/>)}
-        </div>
-      </div>
+      </Router>
     );
   }
 }
--- src/navBar.js
+++ src/navBar.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+  
 export const NavBar = () => {
   return (
     <div className='navBar'>
-      NavBar?
+      <NavLink exact activeClassName='selected' to='/'>Home</NavLink>
+      <NavLink activeClassName='selected' to='/create-idea'>Create Idea</NavLink>
+      <NavLink activeClassName='selected' to='/ideas'>View Ideas</NavLink>
+      <NavLink activeClassName='selected' to='/create-thought'>Create Thought</NavLink>
+      <NavLink activeClassName='selected' to='/list-thoughts'>Show Thoughts</NavLink>
     </div>
   );
 }
